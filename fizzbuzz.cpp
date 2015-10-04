6ff8454db9033e66ce58fd836d0f51c7316c2c94
pk400 <joelaro@outlook.com>
2015-10-03 21:18:54 -0400

    Added iterative fizzbuzz

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -3,29 +3,50 @@
 
 using namespace std;
 
+void iterativeFizzbuzz(int k) {
+	for(int i = 0; i <= k; i++) {
+		if(i % 3 == 0 && i % 5 == 0) {
+			puts("fizzbuzz");
+		} else if(i % 3 == 0) {
+			puts("fizz");
+		} else if(i % 5 == 0) {
+			puts("buzz");
+		} else {
+			cout << i << '\n';
+		}
+	}
+}
+
 int recursiveFizzbuzz(int i)
 {
 	if(i > 1) {
 		fizzbuzz(--i);
-		if(i % 3 == 0 && i % 5 == 0)
-			{ puts("fizzbuzz"); }
-		else if(i % 3 == 0)
-			{ puts("fizz"); }
-		else if(i % 5 == 0)
-			{ puts("buzz"); }
-		else
-			{ cout << i << '\n'; }
+		if(i % 3 == 0 && i % 5 == 0) {
+			puts("fizzbuzz");
+		}
+		else if(i % 3 == 0) {
+			puts("fizz");
+		}
+		else if(i % 5 == 0) {
+			puts("buzz");
+		}
+		else {
+			cout << i << '\n';
+		}
 		return i;
 	}
 }
 
 int main(void)
 {
-	// Tiny fizzbuzz
-	for(int i=0;i++<100;printf(i%3==0?i%5==0?"fizzbuzz\n":"fizz\n":i%5==0?"buzz\n":"%d\n",i));
-	
+	// Iterative fizzbuzz
+	iterativeFizzbuzz(100);
+
 	// Recursive fizzbuzz
 	recursiveFizzbuzz(100);
+	
+	// Tiny fizzbuzz
+	for(int i=0;i++<100;printf(i%3==0?i%5==0?"fizzbuzz\n":"fizz\n":i%5==0?"buzz\n":"%d\n",i));
 	
 	return 0;
 }
